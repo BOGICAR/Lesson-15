795ece51189ebfa6caf7d643a6409af35122ec19
BOGICAR <85548214+BOGICAR@users.noreply.github.com>
2021-08-12 15:54:32 +0300

    Lesson №15
    Task 1

---
+++ task_number_15_1.py
@@ -0,0 +1,30 @@
+import re
+import csv
+user_input = input()
+
+
+def check_car(u_input):
+    if bool(re.fullmatch(r'([А-ЯA-Z]){2}\d{4}[А-ЯA-Z]{2}', u_input)) is True:
+        return True
+    else:
+        return False
+
+
+def check_car_csv(u_input):
+    with open('ua_auto.csv', 'r+', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        list_ = []
+        for i in reader:
+            list_.append(i)
+        for i in list_:
+            if bool(i[1] or i[2] == u_input[:2]) is True:
+                return True
+            else:
+                return False
+
+
+if check_car(user_input) == check_car_csv(user_input):
+    print('The car number is correct')
+else:
+    print('The car number is not correct')
+    
