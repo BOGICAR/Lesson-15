5fd18894e1510707ab5e939731c44aec48435cf3
BOGICAR <85548214+BOGICAR@users.noreply.github.com>
2021-08-19 14:23:51 +0300

    Lesson №15
    Task 1

--- task_number_15_1.py
+++ task_number_15_1.py
@@ -10,21 +10,24 @@ def check_car(u_input):
         return False
 
 
-def check_car_csv(u_input):
+def open_csv():
     with open('ua_auto.csv', 'r+', encoding='utf-8') as file:
         reader = csv.reader(file)
         list_ = []
         for i in reader:
             list_.append(i)
-        for i in list_:
-            if bool(i[1] or i[2] == u_input[:2]) is True:
-                return True
-            else:
-                return False
+        return list_
 
 
-if check_car(user_input) == check_car_csv(user_input):
+def check_car_csv(list_, u_input):
+    for i in list_:
+        if bool(i[1] or i[2] == u_input[:2]) is True:
+            return True
+        else:
+            return False
+
+
+if check_car(user_input) == check_car_csv(open_csv(), user_input):
     print('The car number is correct')
 else:
     print('The car number is not correct')
-    
