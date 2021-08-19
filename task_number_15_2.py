65f93d0b450dd4aaa3a5484c45bd3f2ab65b70f5
BOGICAR <85548214+BOGICAR@users.noreply.github.com>
2021-08-19 15:18:09 +0300

    Lesson №15
    Task 2

--- task_number_15_2.py
+++ task_number_15_2.py
@@ -3,13 +3,12 @@ user_input = input()
 
 
 def task_15_2(u_input):
-    match = re.sub(r'(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*',
-                   r'(+38) 0\2\3 \4\5\6-\7\8-\9\10', u_input)
-    print(match)
-    if bool(re.fullmatch(r'\(\+38\)\s0\d{2}\s\d{3}-\d{2}-\d{2}', match)) is True:
-        return print('Ok')
+    match = re.sub(r'.*(\d{3}).*(\d{3}).*(\d{2}).*(\d{2}).*',
+                   r'(+38) \1 \2-\3-\4', u_input)
+    if len(match) > 9:
+        return match
     else:
-        return print('Failed to recognize number')
+        return 'Failed to recognize number'
 
 
-task_15_2(user_input)
+print(task_15_2(user_input))
